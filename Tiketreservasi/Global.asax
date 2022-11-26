df636f610ff3103c44bc94dba934de7290b08842
Didin Yahya <didinnuryahya@gmail.com>
2022-11-26 10:46:42 +0700

    first commit

---
+++ Tiketreservasi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tiketreservasi.MvcApplication" Language="C#" %>
---
+++ Tiketreservasi/Security/AuthorizationFilterUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace Tiketreservasi.Security
+{
+    public class AuthorizationFilterUser : AuthorizeAttribute,
+    IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if
+            (filterContext.ActionDescriptor.IsDefined(typeof(AllowAnonymousAttribute), true) ||
+                filterContext.ActionDescriptor.ControllerDescriptor.IsDefined(typeof
+                (AllowAnonymousAttribute), true))
+            {
+
+                return;
+            }
+            // Check for authorization
+            if (HttpContext.Current.Session["user"] == null)
+            {
+                filterContext.Result = filterContext.Result =
+                new HttpUnauthorizedResult();
+            }
+        }
+    }
+}
+
